30b97867c7de668eff2cc6d2711256f581145ec7
lakmal <lakmalmabm@gmail.com>
2025-11-24 12:40:44 +0530

    fixed build issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: [
+    'pino',
+    'thread-stream',
+    '@walletconnect/logger',
+  ],
+  webpack: (config, { isServer }) => {
+    // Exclude problematic packages from client bundle
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
